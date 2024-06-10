831fc6c4df3f83f060fe82ba689c77cb0fe2f7eb
Atarame0 <100781810+Atarame0@users.noreply.github.com>
2024-06-10 22:07:59 +0300

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/f9VQueIp9pTsflY4/scene.splinecode');
